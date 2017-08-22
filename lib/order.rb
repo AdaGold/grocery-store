0201e3d337b54b1ea40cd0af84f1ab73999d8c73
kee nam <nam.keeyeon@gmail.com>
2017-08-22 14:07:26 -0700

    Added to initialize, total, add_product, and remove_product methods

--- lib/order.rb
+++ lib/order.rb
@@ -5,14 +5,39 @@ module Grocery
     def initialize(id, products)
       @id = id
       @products = products
+
+      if @products == nil
+        @products == 0
+      end
     end
 
     def total
       # TODO: implement total
+      total = 0
+      @products.each do |product, price|
+        total = total + price
+      end
+      total = total + (total * 0.075).round(2)
+      return total
     end
 
     def add_product(product_name, product_price)
       # TODO: implement add_product
+      if @products.keys.include?(product_name)
+        return false
+      else
+        @products[product_name] = product_price
+        return true
+      end
+    end
+
+    def remove_product(product_name)
+      if @products.keys.include?(product_name)
+        @products.delete(product_name)
+        return true
+      else
+        return false
+      end
     end
   end
 end
