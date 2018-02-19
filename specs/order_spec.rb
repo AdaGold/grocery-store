cec389ff1e2475709b55b553bb74461f5a0046e6
HughesErika <Seattlesilencer>
2018-02-19 11:40:53 -0800

    Order.all tests passing

--- specs/order_spec.rb
+++ specs/order_spec.rb
@@ -78,7 +78,6 @@ describe "Order Wave 1" do
   end
 end
 
-# TODO: change 'xdescribe' to 'describe' to run these tests
 describe "Order Wave 2" do
   describe "Order.all" do
     it "Returns an array of all orders" do
@@ -104,7 +103,7 @@ describe "Order Wave 2" do
     end
   end
 
-  xdescribe "Order.find" do
+  describe "Order.find" do
     it "Can find the first order from the CSV" do
       # TODO: Your test code here!
     end
