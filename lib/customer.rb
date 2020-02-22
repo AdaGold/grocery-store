cddd4270d1ca3ba0e3f019b7d5a0b5c2139f6c14
charlotte adams <lucky.seattle.monkey@gmail.com>
2020-02-22 11:11:47 -0800

    all of the basic requirements complete - all tests pass

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,19 +1,34 @@
+require 'csv'
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
 
-def initialize(id, email, address = {})
-  # id is a number
-  @id= id
-  # email is a string
-  @email = email
-  # delivery_address is a hash
-  @address = address
-end
+  def initialize(id, email, address = {})
+    @id= id
+    @email = email
+    @address = address
+  end
+
+
+  def self.all
+    customers = []
+    CSV.read('./data/customers.csv').each do |customer|
+      each_customer = (Customer.new(customer[0].to_i, customer[1],
+      {:street => customer[2], :city => customer[3], :state => customer[4], :zip => customer[5]}))
+      customers.push(each_customer)
+    end
+    return customers
+  end
+
+  def self.find(id)
+    Customer.all.each do |customer|
+      if customer.id == id
+        return customer 
+      end 
+    end
+    return nil
+  end
 
 
 end
-
-
-
-
--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,6 @@
+require 'csv'
+require 'awesome_print'
+
 class Order
 
   attr_reader :id
@@ -8,7 +11,6 @@ class Order
     @products = products
     @customer = customer
     @fulfillment_status = fulfillment_status
-    shipping_statuses = [:paid, :processing, :shipped, :complete, :pending]
     valid_statuses =  [:pending, :paid, :processing, :shipped,:complete]
       unless valid_statuses.include?(fulfillment_status)
         raise ArgumentError, 'Fulfillment Status is an invalid entry!'
@@ -33,5 +35,45 @@ class Order
     return products
     end
   end
+  
+  # str = "Iceberg lettuce:88.51;Rice paper:66.35;Amaranth:1.5;Walnut:65.26"
+  def self.product_hash_flatten(str)
+    split_str = str.split(';')
+    split_str.map! do |el|
+      el.split(':')
+    end
+    # create hash :name => price
+    product_hash = {}
+    split_str.each do|product|
+      product_hash[product[0]] = product[1].to_f
+    end
+    return product_hash
+  end
+
+
+# self.all - returns a collection of Order instances, representing all of the Orders described in the CSV file
+ 
+  def self.all
+    orders = []
+    CSV.read('./data/orders.csv').each do |order|
+      each_order = Order.new(order[0].to_i, Order.product_hash_flatten(order[1]), Customer.find(order[2].to_i), order[3].to_sym)
+      orders.push(each_order)
+    end
+    return orders 
+  end
+
+  
+  def self.find(id)
+    Order.all.each do |order|
+      if order.id == id
+        return order 
+      end 
+    end
+    return nil
+  end
 
 end
+
+
+#   ap order
+# end
