6b9a50744fc094ae880e3c9e2970af5281149b72
charlotte adams <lucky.seattle.monkey@gmail.com>
2020-02-22 12:08:39 -0800

    create and pass tests

--- test/order_test.rb
+++ test/order_test.rb
@@ -140,22 +140,45 @@ end
       expect(order.fulfillment_status).must_equal fulfillment_status
     end
 
-    it "Returns accurate information about the last order" do
-      # TODO: Your test code here!
-    end
+      it "Returns accurate information about the last order" do
+        id = 100
+        products = {
+          "Amaranth" => 83.81,
+          "Smoked Trout" => 70.6,
+          "Cheddar" => 5.63
+        }
+        customer_id = 20
+        fulfillment_status = :pending
+  
+        order = Order.all.last
+  
+        # Check that all data was loaded as expected
+        expect(order.id).must_equal id
+        expect(order.products).must_equal products
+        expect(order.customer).must_be_kind_of Customer
+        expect(order.customer.id).must_equal customer_id
+        expect(order.fulfillment_status).must_equal fulfillment_status
+      end
   end
 
   describe "Order.find" do
     it "Can find the first order from the CSV" do
-      # TODO: Your test code here!
+      first = Order.find(1)
+
+      expect(first).must_be_kind_of Order
+      expect(first.id).must_equal 1
     end
 
     it "Can find the last order from the CSV" do
-      # TODO: Your test code here!
+      last = Order.find(100)
+
+      expect(last).must_be_kind_of Order
+      expect(last.id).must_equal 100
     end
 
     it "Returns nil for an order that doesn't exist" do
-      # TODO: Your test code here!
+      expect(Order.find(101)).must_be_nil
+
     end
   end
 end
